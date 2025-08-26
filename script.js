2ebe1b205c1cb93a33a90f8ec083337b72182707
Ailhay <ailhay@ailhay.com>
2025-08-26 15:59:48 -0400

    typo!

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function onPageLoaded() {
   	console.log (audio)
 }
 
-const audioArray = ['audio/100 Sunny.mp3', 'audio/An Archaeic Resonance.mp3', 'audio/Bargain Bin Boys.mp3', 'audio/Finding Shaptes in the Clouds.mp3', 'audio/Forest Chillin.mp3', 'audio/Good for Health, Bad for Imagination.mp3', 'audio/Lost,Then Found!.mp3', 'audio/Pyrefly Forest - Cats Cradle.mp3', 'audio/Room for 4.mp3', 'audio/Spaces Inbetween..mp3', 'audio/The Last Resort.mp3', 'audio/Trees....mp3', 'audio/Where We Used to Play.mp3'];
+const audioArray = ['audio/100 Sunny.mp3', 'audio/An Archaeic Resonance.mp3', 'audio/Bargain Bin Boys.mp3', 'audio/Finding Shaptes in the Clouds.mp3', 'audio/Forest Chillin.mp3', 'audio/Good for Health, Bad for Imagination.mp3', 'audio/Lost, Then Found!.mp3', 'audio/Pyrefly Forest - Cats Cradle.mp3', 'audio/Room for 4.mp3', 'audio/Spaces Inbetween..mp3', 'audio/The Last Resort.mp3', 'audio/Trees....mp3', 'audio/Where We Used to Play.mp3'];
     
 function playAudio(){
   	const audioIndex = Math.floor(Math.random() * audioArray.length);
