a7f51dd8def7676f39aaf17825a9d3fa769c3120
Ailhay <ailhay@ailhay.com>
2025-08-27 12:54:49 -0400

    player updates
    it now is close! All I really need to do now is have it work seperately from the rest so it won't play in multiple tabs and all be synced but idk how to yet!

--- main.js
+++ main.js
@@ -104,45 +104,29 @@ function onPageLoaded() {
 
 function loadTrack(track_index) {
   // Clear the previous seek timer
-  clearInterval(updateTimer);
-  resetValues();
+  //clearInterval(updateTimer);
+  //resetValues();
 
   // Load a new track
   curr_track.src = track_list[track_index].path;
   curr_track.load();
 
   // Update details of the track
-  track_art.style.backgroundImage = 
-     "url(" + track_list[track_index].image + ")";
+  //track_art.style.backgroundImage = 
+     //"url(" + track_list[track_index].image + ")";
   track_name.textContent = track_list[track_index].name;
   track_artist.textContent = track_list[track_index].artist;
-  now_playing.textContent = 
-     "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+  //now_playing.textContent = 
+  //   "PLAYING " + (track_index + 1) + " OF " + track_list.length;
 
   // Set an interval of 1000 milliseconds
   // for updating the seek slider
-  updateTimer = setInterval(seekUpdate, 1000);
+  //updateTimer = setInterval(seekUpdate, 1000);
 
   // Move to the next track if the current finishes playing
   // using the 'ended' event
   curr_track.addEventListener("ended", nextTrack);
 
-  // Apply a random background color
-  random_bg_color();
-}
-
-function random_bg_color() {
-  // Get a random number between 64 to 256
-  // (for getting lighter colors)
-  let red = Math.floor(Math.random() * 256) + 64;
-  let green = Math.floor(Math.random() * 256) + 64;
-  let blue = Math.floor(Math.random() * 256) + 64;
-
-  // Construct a color with the given values
-  let bgColor = "rgb(" + red + ", " + green + ", " + blue + ")";
-
-  // Set the background to the new color
-  document.body.style.background = bgColor;
 }
 
 // Function to reset all values to their default
@@ -184,9 +168,11 @@ function pauseTrack() {
 function nextTrack() {
   // Go back to the first track if the
   // current one is the last in the track list
-  if (track_index < track_list.length - 1)
-    track_index += 1;
-  else track_index = 0;
+  //if (track_index < track_list.length - 1)
+  //  track_index += 1;
+  //else track_index = 0;
+  
+  track_index = Math.floor(Math.random() * track_list.length);
 
   // Load and play the new track
   loadTrack(track_index);
@@ -254,4 +240,5 @@ function seekUpdate() {
 
 // Load the first track in the tracklist
 setVolume();
+nextTrack();
 loadTrack(track_index);
